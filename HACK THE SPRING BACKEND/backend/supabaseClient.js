283193ad29aa471bec92b0ac06246cd711f6ddef
Jay Kanani <jaykanani07@gmail.com>
2026-04-09 21:05:21 +0530

    removed node_modules

--- HACK THE SPRING BACKEND/backend/supabaseClient.js
+++ HACK THE SPRING BACKEND/backend/supabaseClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://acbiuzxkxnsbqyekytdi.supabase.co';
-const supabaseKey = 'sb_publishable_i1HdlKcDVO9M61j5hb1DFA_hyLF2z2F';
+    const supabaseUrl = 'https://acbiuzxkxnsbqyekytdi.supabase.co';
+    const supabaseKey = 'sb_publishable_i1HdlKcDVO9M61j5hb1DFA_hyLF2z2F';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
